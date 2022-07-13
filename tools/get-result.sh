9069cb7e1210e9582e658be953260205a667b075
taosy <shaoyu.tao@intel.com>
2022-07-13 09:18:50 -0700

    update

--- tools/get-result.sh
+++ tools/get-result.sh
@@ -8,13 +8,21 @@ CASE_LIST=""
 CORE_COLLECT_APP_MASK=1     
 CORE_RUN_APP_MASK=2     
 
+# Run in 1 cpu
+CASE_LIST_1="bench-memset bench-strcpy bench-memcpy-large bench-sprintf bench-acos bench-asinh bench-exp \
+           bench-log2 bench-sin bench-sincos bench-sqrt bench-tanh bench-pthread_once"     
+
+# Run in 8 cpu
+CASE_LIST_2="bench-malloc-thread bench-thread_create" 
+
+# Run in 4 cpu
+CASE_LIST_3="bench-pthread-locks bench-malloc-simple" 
+
 usage() {
     cat << EOM
 Usage: $(basename "$0") [OPTION]...
   -n rounds of run test
   -f fold of glibc-bench
-  -c case name
-  -R cpumask for Running APP, default is 2
   -C cpumask for Collect data APP, dafault is 1
   -h Show this
 Sample:
@@ -28,11 +36,21 @@ process_args() {
         case "$option" in
             n) ROUNDS=$OPTARG;;
             f) FOLD_OF_GLIBC_BENCH=$OPTARG;;
-            R) CORE_RUN_APP_MASK=$OPTARG;;
             C) CORE_COLLECT_APP_MASK=$OPTARG;;
             h) usage;;
         esac
     done
 }
 
-./rcs_all.sh -f ${FOLD_OF_GLIBC_BENCH} -n ${ROUNDS} -R ${CORE_RUN_APP_MASK} -c 'bench-thread_create' | python3 trans.py
+process_args "$@"
+
+CORE_RUN_APP_MASK=4
+./rcs_all.sh -f ${FOLD_OF_GLIBC_BENCH} -n ${ROUNDS} -R ${CORE_RUN_APP_MASK} -c "${CASE_LIST_1}"
+
+CORE_RUN_APP_MASK="4-11"
+./rcs_all.sh -f ${FOLD_OF_GLIBC_BENCH} -n ${ROUNDS} -R ${CORE_RUN_APP_MASK} -c "${CASE_LIST_2}"
+
+CORE_RUN_APP_MASK="4-7"
+./rcs_all.sh -f ${FOLD_OF_GLIBC_BENCH} -n ${ROUNDS} -R ${CORE_RUN_APP_MASK} -c "${CASE_LIST_3}"  
+
+
